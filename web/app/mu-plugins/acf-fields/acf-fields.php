72de448dc15e8ec69b8d0e2735b535c9f6d785ad
Ivaylo Sholekov <sholeka@gmail.com>
2020-09-09 17:12:15 +0300

    Archive product fields added

--- web/app/mu-plugins/acf-fields/acf-fields.php
+++ web/app/mu-plugins/acf-fields/acf-fields.php
@@ -652,6 +652,56 @@ function cmpso_acf_fields() {
     endif; // END Question
 
 
+    if( function_exists('acf_add_local_field_group') ): // START Products Archive
+
+        acf_add_local_field_group(array(
+            'key' => 'group_5f560fec16078',
+            'title' => 'Products Archive',
+            'fields' => array(
+                array(
+                    'key' => 'field_5f561008940b4',
+                    'label' => 'Categories',
+                    'name' => 'categories',
+                    'type' => 'taxonomy',
+                    'instructions' => '',
+                    'required' => 0,
+                    'conditional_logic' => 0,
+                    'wrapper' => array(
+                        'width' => '',
+                        'class' => '',
+                        'id' => '',
+                    ),
+                    'taxonomy' => 'product_cat',
+                    'field_type' => 'checkbox',
+                    'add_term' => 1,
+                    'save_terms' => 0,
+                    'load_terms' => 0,
+                    'return_format' => 'object',
+                    'multiple' => 0,
+                    'allow_null' => 0,
+                ),
+            ),
+            'location' => array(
+                array(
+                    array(
+                        'param' => 'page',
+                        'operator' => '==',
+                        'value' => '285',
+                    ),
+                ),
+            ),
+            'menu_order' => 0,
+            'position' => 'normal',
+            'style' => 'default',
+            'label_placement' => 'top',
+            'instruction_placement' => 'label',
+            'hide_on_screen' => '',
+            'active' => true,
+            'description' => '',
+        ));
+        
+    endif; // END Products Archive
+
     if( function_exists('acf_add_local_field_group') ):  // START Product
 
         acf_add_local_field_group(array(
